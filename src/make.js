7635c48af1e58b948d933214790bcde275ff6c93
Rory Wilson <rorywilson@Rorys-MacBook-Pro.local>
2020-05-27 17:35:31 +0100

    Added make script to build grid views

---
+++ src/make.js
@@ -0,0 +1,77 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fsp = require('fs').promises;
+const moment = require('moment');
+const path = require('path');
+
+const LAYOUTS = {
+    3: '0_0|0_h0|0_h0+h1',
+    4: '0_0|0_h0|w0_0|w0_h0',
+    9: '0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1',
+    36: '0_0|w0_0|w0+w1_0|w0+w1+w2_0|w0+w1+w2+w3_0|w0+w1+w2+w3+w4_0|0_h0|w0_h0|w0+w1_h0|w0+w1+w2_h0|w0+w1+w2+w3_h0|w0+w1+w2+w3+w4_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1|w0+w1+w2_h0+h1|w0+w1+w2+w3_h0+h1|w0+w1+w2+w3+w4_h0+h1|0_h0+h1+h2|w0_h0+h1+h2|w0+w1_h0+h1+h2|w0+w1+w2_h0+h1+h2|w0+w1+w2+w3_h0+h1+h2|w0+w1+w2+w3+w4_h0+h1+h2|0_h0+h1+h2+h3|w0_h0+h1+h2+h3|w0+w1_h0+h1+h2+h3|w0+w1+w2_h0+h1+h2+h3|w0+w1+w2+w3_h0+h1+h2+h3|w0+w1+w2+w3+w4_h0+h1+h2+h3|0_h0+h1+h2+h3+h4|w0_h0+h1+h2+h3+h4|w0+w1_h0+h1+h2+h3+h4|w0+w1+w2_h0+h1+h2+h3+h4|w0+w1+w2+w3_h0+h1+h2+h3+h4|w0+w1+w2+w3+w4_h0+h1+h2+h3+h4'
+}
+
+const loadEdit = async (filepath) => {
+    const source = await fsp.readFile(filepath, 'utf8');
+    return JSON.parse(source);
+}
+const run = async (cmd) => {
+    console.log(cmd);
+    return exec(cmd);
+}
+
+const makeGrid = async (cut, sourceDir, destinationPath) => {
+    const sourceFiles = cut.clips.map(clip => path.join(sourceDir, clip));
+    const inputCount = sourceFiles.length;
+    const { start, duration } = cut;
+    const layout = LAYOUTS[inputCount];
+
+    console.log(`Making a grid of ${sourceFiles.length} images from ${start} for ${duration} seconds, saving to ${destinationPath}`);
+    const startString = moment().startOf('day').seconds(start).format('HH:mm:ss');
+    const files = sourceFiles.map(file => `-ss ${startString} -t ${duration} -i ${file}`).join(' ');
+
+    const inputMatrix = [];
+    for (j = 0; j < sourceFiles.length; j++) {
+        inputMatrix.push(j);
+    }
+    const inputMatrixString = inputMatrix.map(x => `[${x}:v] setpts=PTS-STARTPTS, scale=qvga [a${x}];`).join('');
+    const inputLabels = inputMatrix.map(x => `[a${x}]`).join('');
+    return await run(`ffmpeg -y ${files} -filter_complex "${inputMatrixString}${inputLabels}xstack=inputs=${inputCount}:layout=${layout}[v]" -map "[v]" ${destinationPath}`);
+}
+
+const concat = async (sourceFiles, workingDir, destinationPath) => {
+    console.log(`Concatenating ${sourceFiles.length} saving to ${destinationPath}`);
+    const fileListPath = path.join(workingDir, 'filelist.txt');
+    await fsp.writeFile(fileListPath, sourceFiles.map(file => `file '${file}'`).join('\n'));
+
+    return await run(`ffmpeg -y -f concat -safe 0 -i ${fileListPath} -c copy ${destinationPath}`);
+}
+
+const make = async (args) => {
+    const sourceDir = path.join(__dirname, '../', args[0]);
+
+    const workingDir = path.join(__dirname, '../', 'working');
+    const outputDir = path.join(__dirname, '../', 'output');
+
+    const editJson = await loadEdit(args[1]);
+    const tempFiles = [];
+
+    // cut
+    for (i = 0; i < editJson.cuts.length; i++) {
+        const cut = editJson.cuts[i];
+        const destinationFile = path.join(workingDir, `${i}.mp4`);
+        tempFiles.push(destinationFile);
+        await makeGrid(cut, sourceDir, destinationFile);
+    }
+
+    // join
+    const destinationFile = path.join(outputDir, `final.mp4`);
+    await concat(tempFiles, workingDir, destinationFile)
+}
+
+var args = process.argv.slice(2);
+try {
+    make(args);
+} catch (error) {
+    console.error(error);
+}
