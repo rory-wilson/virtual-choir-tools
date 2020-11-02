1b200e33dcca6d815ace4be1db46224d4a6d5c47
Campbell Vertesi <campbell@vertesi.com>
2020-11-02 16:15:03 +0100

    Validate graceful handling of files with silence at the end

--- __tests__/silences.spec.js
+++ __tests__/silences.spec.js
@@ -1,3 +1,4 @@
+jest.setTimeout(10000)
 console.log = jest.fn();
 console.error = jest.fn();
 const silence = require('../src/silences.js');
@@ -6,22 +7,43 @@ const outputFileName = "output/detected-silence.txt"
 const inputFileName = "__tests__/assets/silence_detection.mp4"
 
 describe("Silence detection", () => {
-  jest.setTimeout(10000)
+  let inputFileName = "__tests__/assets/silence_detection.mp4"
+
   test("it should find silence start and end", () => {
     return silence.detectSilence(inputFileName, outputFileName).then(output => {
+      checkFfmpegStderr(output.stderr)
+      return fsp.readFile(outputFileName, 'utf8').then(outputFileContents => {
+        checkFileContents(outputFileContents)
+      })
+    });
+  })
+  test("it should gracefully handle videos that are silent at the end", () => {
+    let inputFileName = "__tests__/assets/silence_detection_no_end.mp4"
+
+    return silence.detectSilence(inputFileName, outputFileName).then(output => {
       // Check FFmpeg stderr outputs.
-      expect(output.stderr).toMatch(/silence_start: ([\d.\d]+)/g)
-      expect(output.stderr).toMatch(/silence_end: ([\d.\d]+)/g)
-      expect(output.stderr).toMatch(/silence_duration: ([\d.\d]+)/g)
+      checkFfmpegStderr(output.stderr)
       // Check file contents.
       return fsp.readFile(outputFileName, 'utf8').then(outputFileContents => {
-        expect(outputFileContents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
-        expect(outputFileContents).toMatch(/lavfi.silence_end=([\d\.\d]+)/g)
+        checkFileContents(outputFileContents)
       })
     });
   })
 });
 
-afterAll(() => {
+// Check FFmpeg stderr outputs.
+const checkFfmpegStderr = async (stderr) => {
+  expect(stderr).toMatch(/silence_start: ([\d.\d]+)/g)
+  expect(stderr).toMatch(/silence_end: ([\d.\d]+)/g)
+  expect(stderr).toMatch(/silence_duration: ([\d.\d]+)/g)
+}
+
+// Check file contents.
+const checkFileContents = async (contents) => {
+  expect(contents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
+  expect(contents).toMatch(/lavfi.silence_end=([\d\.\d]+)/g)
+}
+
+afterEach(() => {
   const source = fsp.unlink(outputFileName);
 });
