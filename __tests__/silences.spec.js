80e64b283c70132caceab03a0f0be42c67cae4ab
Campbell Vertesi <campbell@vertesi.com>
2020-11-02 17:19:50 +0100

    Expect no end timestamp when there is no silence end

--- __tests__/silences.spec.js
+++ __tests__/silences.spec.js
@@ -12,12 +12,13 @@ describe("Silence detection", () => {
   test("it should find silence start and end", () => {
     return silence.detectSilence(inputFileName, outputFileName).then(output => {
       checkFfmpegStderr(output.stderr)
+      // Check file contents.
       return fsp.readFile(outputFileName, 'utf8').then(outputFileContents => {
-        checkFileContents(outputFileContents)
-      })
+        expect(outputFileContents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
+        expect(outputFileContents).toMatch(/lavfi.silence_end=([\d\.\d]+)/g)      })
     });
   })
-  test("it should gracefully handle videos that are silent at the end", () => {
+  test("it should not include an endtime for videos that are silent at the end", () => {
     let inputFileName = "__tests__/assets/silence_detection_no_end.mp4"
 
     return silence.detectSilence(inputFileName, outputFileName).then(output => {
@@ -25,7 +26,8 @@ describe("Silence detection", () => {
       checkFfmpegStderr(output.stderr)
       // Check file contents.
       return fsp.readFile(outputFileName, 'utf8').then(outputFileContents => {
-        checkFileContents(outputFileContents)
+        expect(outputFileContents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
+        expect(outputFileContents).not.toMatch(/lavfi.silence_end=([\d\.\d]+)/g)      
       })
     });
   })
@@ -36,12 +38,6 @@ const checkFfmpegStderr = (stderr) => {
   expect(stderr).toMatch(/silence_start: ([\d.\d]+)/g)
   expect(stderr).toMatch(/silence_end: ([\d.\d]+)/g)
   expect(stderr).toMatch(/silence_duration: ([\d.\d]+)/g)
-}
-
-// Check file contents.
-const checkFileContents = (contents) => {
-  expect(contents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
-  expect(contents).toMatch(/lavfi.silence_end=([\d\.\d]+)/g)
 }
 
 afterEach(() => {
