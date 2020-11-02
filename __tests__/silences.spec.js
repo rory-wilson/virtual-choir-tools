254ec881742b2522a15cbb56b9ff3044ca4c57d8
Campbell Vertesi <campbell@vertesi.com>
2020-11-02 15:28:30 +0100

    Add validation of silence file contents

--- __tests__/silences.spec.js
+++ __tests__/silences.spec.js
@@ -11,6 +11,11 @@ describe("Silence detection", () => {
       expect(output.stderr).toMatch(new RegExp('silence_start: 0.725208'))
       expect(output.stderr).toMatch(new RegExp('silence_end: 16.4535'))
       expect(output.stderr).toMatch(new RegExp('silence_duration: 15.7282'))
+      // Check file contents.
+      return fsp.readFile(outputFileName, 'utf8').then(outputFileContents => {
+        expect(outputFileContents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
+        expect(outputFileContents).toMatch(/lavfi.silence_end=([\d\.\d]+)/g)
+      })
     });
   })
 });
