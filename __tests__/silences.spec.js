fd624f307d47209c0bb13db3e6848a2ba4b94e9f
Campbell Vertesi <campbell@vertesi.com>
2020-11-02 21:04:52 +0100

    Restructure tests for clarity

--- __tests__/silences.spec.js
+++ __tests__/silences.spec.js
@@ -12,11 +12,13 @@ describe("Silence detection", () => {
   test("it should find silence start and end", () => {
     return silence.detectSilence(inputFileName, outputFileName).then(output => {
       checkFfmpegStderr(output.stderr)
-      // Check file contents.
-      return fsp.readFile(outputFileName, 'utf8').then(outputFileContents => {
-        expect(outputFileContents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
-        expect(outputFileContents).toMatch(/lavfi.silence_end=([\d\.\d]+)/g)      })
     });
+  })
+  test("it should record the silence start and end times in the output file", () => {
+    return fsp.readFile(outputFileName, 'utf8').then(outputFileContents => {
+      expect(outputFileContents).toMatch(/lavfi.silence_start=([\d.\d]+)/g)
+      expect(outputFileContents).toMatch(/lavfi.silence_end=([\d\.\d]+)/g)
+    })
   })
   test("it should not include an endtime for videos that are silent at the end", () => {
     let inputFileName = "__tests__/assets/silence_detection_no_end.mp4"
@@ -31,14 +33,17 @@ describe("Silence detection", () => {
       })
     });
   })
+});
+
+describe("Get clip duration as a fallback measure", () => {
   test("it should be able to get clip duration", () => {
     let inputFileName = "__tests__/assets/silence_detection_no_end.mp4"
     return silence.getClipDuration(inputFileName).then(duration => {
       expect(duration).toBe("8.853000")
     })
-
   })
-});
+})
+
 
 // Check FFmpeg stderr outputs.
 const checkFfmpegStderr = (stderr) => {
@@ -47,6 +52,6 @@ const checkFfmpegStderr = (stderr) => {
   expect(stderr).toMatch(/silence_duration: ([\d.\d]+)/g)
 }
 
-afterEach(() => {
+afterAll(() => {
   const source = fsp.unlink(outputFileName);
 });
